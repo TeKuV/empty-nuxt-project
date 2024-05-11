40e06558751f1de67b30e922b5a91ffeeebde38a
unknown <teugaulrichgmail.com>
2024-05-11 08:42:18 +0100

    Configuration of routing on app

--- app.vue
+++ app.vue
@@ -2,5 +2,8 @@
   <div>
     <p class="text-red-300">A toi de jouer, l'image proviens du dossiers assets</p>
     <img src="@/assets/a1.jpeg" alt="">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
